7ac856f7de2e97c0b4cbc53b7135b37e36ba942f
Lisharabbit <lisaqiu763@gmail.com>
2018-06-04 00:21:45 +1000

    delete comments in urls.py

--- parkdata/urls.py
+++ parkdata/urls.py
@@ -6,33 +6,9 @@ from . import models
 
 app_name = 'parkdata'
 urlpatterns = [
-    # path('', views.index, name='index'),
+   
     path('predict',views.predict, name = 'predict'),
     path('lastweekdata',views.lastWeekdata, name = 'lastWeekdata'),
     path('suggestbays',views.suggestbays, name = 'suggestbays'),
-    # path('<int:parkingdata_id>/', views.detail, name='detail'),
-    # path('detail/',views.detail,name = 'detial'),
-    # path('search/', view.search,),
-    # path('runJob/',views.runJob),
-    # url(r'^runJob/$',views.run_job),
-    # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
 
-# from django.conf.urls import url
-# from Api import views
-#
-# urlpatterns = [
-#     url(r'^runJob/$',views.run_job),
-# ]
-
-# Serializers define the API representation.
-
-
-# Routers provide an easy way of automatically determining the URL conf.
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
-# urlpatterns = [
-#     url(r'^', include(router.urls)),
-#     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-# ]
